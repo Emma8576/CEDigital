52d17834d1792cb3ed9ce2cbd6e990afe4f646fc
JoaquinRamirezS <jramirezsequeira@gmail.com>
2025-05-20 22:01:11 -0600

    Conteo de Grupos Activos por semestre

--- api/CEDigital.API/obj/Debug/net9.0/CEDigital.API.AssemblyInfo.cs
+++ api/CEDigital.API/obj/Debug/net9.0/CEDigital.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CEDigital.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5eaa1ddb950892381aa7b7bfcccc9d732970568")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ebeb7efcae47e15cef257e5a5048ea9a3e86784")]
 [assembly: System.Reflection.AssemblyProductAttribute("CEDigital.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CEDigital.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
